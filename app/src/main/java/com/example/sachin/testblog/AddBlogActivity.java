e8359b8cfd989c834280309e45f4f146dd85c918
Sachindran <sachindran1991@gmail.com>
2014-09-08 02:12:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sachin/testblog/AddBlogActivity.java
@@ -0,0 +1,80 @@
+package com.example.sachin.testblog;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import java.text.ParseException;
+
+
+public class AddBlogActivity extends Activity {
+
+    private TextView postContent;
+    private Button postButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_blog);
+
+        postContent = ((EditText)findViewById(R.id.post_message));
+        postButton = ((Button)findViewById(R.id.post_send));
+     }
+
+    public void sendMessage(View v)
+    {
+        ParseObject post = new ParseObject("Post");
+        post.put("postContent",postContent.getText().toString());
+
+        ParseUser user = new ParseUser();
+        user.setUsername("Sachin");
+        user.put("Position","New Orleans");
+
+        post.put("author",user.getUsername());
+
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(com.parse.ParseException e) {
+            if (e == null) {
+                    setResult(RESULT_OK);
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Error saving: " + e.getMessage(),
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_blog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
