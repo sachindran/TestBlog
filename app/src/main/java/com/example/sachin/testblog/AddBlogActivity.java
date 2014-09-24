e46b058cb1c3aa5a05d20421d1730b7bf89ad63f
Sachindran <sachindran1991@gmail.com>
2014-09-24 13:11:28 -0500

    Location services working. Post stored with location

--- app/src/main/java/com/example/sachin/testblog/AddBlogActivity.java
+++ app/src/main/java/com/example/sachin/testblog/AddBlogActivity.java
@@ -1,7 +1,18 @@
 package com.example.sachin.testblog;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,18 +21,30 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SupportErrorDialogFragment;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.model.LatLng;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import java.text.ParseException;
 
 
-public class AddBlogActivity extends Activity {
+public class AddBlogActivity extends FragmentActivity implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener  {
 
     private TextView postContent;
     private Button postButton;
+    private LatLng currPosition;
+    LocationClient mLocationClient;
+    Location mCurrentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +53,181 @@ public class AddBlogActivity extends Activity {
 
         postContent = ((EditText)findViewById(R.id.post_message));
         postButton = ((Button)findViewById(R.id.post_send));
+        mLocationClient = new LocationClient(this, this, this);
      }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+        mLocationClient.connect();
+        Toast.makeText(this,"Connection successful",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+
+
+    // Global constants
+    /*
+     * Define a request code to send to Google Play services
+     * This code is returned in Activity.onActivityResult
+     */
+    private final static int
+            CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+    @Override
+    public void onConnected(Bundle bundle)
+    {
+        // Display the connection status
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDisconnected()
+    {
+        Toast.makeText(this, "Disconnected. Please re-connect.",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult)
+    {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            showErrorDialog(connectionResult.getErrorCode());
+        }
+    }
+    /**
+     * Show a dialog returned by Google Play services for the
+     * connection error code
+     *
+     * @param errorCode An error code returned from onConnectionFailed
+     */
+    private void showErrorDialog(int errorCode) {
+
+        // Get the error dialog from Google Play services
+        Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                errorCode,
+                this,
+                CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+        // If Google Play services can provide an error dialog
+        if (errorDialog != null) {
+
+            // Create a new DialogFragment in which to show the error dialog
+            ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+
+            // Set the dialog in the DialogFragment
+            errorFragment.setDialog(errorDialog);
+
+            // Show the error dialog in the DialogFragment
+            errorFragment.show(getFragmentManager(), "TestBlog");
+        }
+    }
+    /*
+ * Handle results returned to the FragmentActivity
+ * by Google Play services
+ */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        // Decide what to do based on the original request code
+        switch (requestCode)
+        {
+
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+            /*
+             * If the result code is Activity.RESULT_OK, try
+             * to connect again
+             */
+                switch (resultCode)
+                {
+                    case Activity.RESULT_OK :
+                    /*
+                     * Try the request again
+                     */
+
+                        break;
+                }
+
+        }
+    }
+
+    private boolean servicesConnected()
+    {
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.
+                        isGooglePlayServicesAvailable(this);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode)
+        {
+            // In debug mode, log the status
+            Log.d("Location Updates",
+                    "Google Play services is available.");
+            // Continue
+            return true;
+            // Google Play services was not available for some reason.
+            // resultCode holds the error code.
+        }
+        else
+        {
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                    resultCode,
+                    this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null)
+            {
+                // Create a new DialogFragment for the error dialog
+
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(getFragmentManager(),"Location Updates");
+            }
+            return false;
+        }
+    }
 
     public void sendMessage(View v)
     {
         ParseObject post = new ParseObject("Post");
         post.put("postContent",postContent.getText().toString());
 
+        mCurrentLocation = mLocationClient.getLastLocation();
+        ParseGeoPoint point = new ParseGeoPoint(mCurrentLocation.getLatitude(),mCurrentLocation.getLongitude());
+        //Toast.makeText(this,"Latitude"+mCurrentLocation,Toast.LENGTH_SHORT).show();
+        post.put("location",point);
 
         post.put("user",ParseUser.getCurrentUser());
         try {
@@ -57,7 +248,7 @@ public class AddBlogActivity extends Activity {
         }
         catch(Exception e)
         {
-               
+
         }
     }
 
@@ -79,4 +270,33 @@ public class AddBlogActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    // Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment
+    {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment()
+        {
+            super();
+            mDialog = null;
+        }
+        // Set the dialog to display
+        public void setDialog(Dialog dialog)
+        {
+            mDialog = dialog;
+        }
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState)
+        {
+            return mDialog;
+        }
+    }
+
 }
+
+
+
+
+
